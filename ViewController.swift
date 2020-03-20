d2f8bccfe0c5bfd6093bb2de12ce916ff4306aa6
lucasfeitosapr <lucasfeitosapr@gmail.com>
2020-03-20 16:54:03 -0400

    Final Commit
    Setting up different sounds for each button, and a fade-out like "animation".

--- ViewController.swift
+++ ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("Start")
+        playSound(withCurrentNote:sender.currentTitle!)
+        sender.alpha = 0.5
+       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            
+            sender.alpha = 1.0
+            print("End")
+            
+        })
+        
+        
+    }
+    
+    func playSound(withCurrentNote: String) {
+        let url = Bundle.main.url(forResource: withCurrentNote, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
     
 
 }
